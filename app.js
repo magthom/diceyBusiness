4a80c77206e9bf73cc21a19df9cb6e69f54c1ec6
Maggie Thompson <maggiethompson@Maggies-MacBook-Air.local>
2020-06-09 11:50:24 -0500

    roll die button working

--- app.js
+++ app.js
@@ -1,36 +1,43 @@
 let container = document.getElementById("container");
 let diceArray = [];
+let rollBtn = document.getElementById("rollBtn")
 
 class Die {
-constructor(value, x, y) {
-    this.div = document.createElement('div');
-    this.div.classList.add('new-die');
-    this.div.style.left = `${x}px`;
-    this.div.style.top = `${y}px`;
-    this.div.style.border = "black, solid, 2px";
-    this.div.appendChild(document.createTextNode(`${value}`));
-    container.appendChild(this.div);
-    this.div.addEventListener('click', () => {
+    constructor(value, x, y) {
+        this.value = value;
+        this.div = document.createElement('div');
+        this.div.classList.add('new-die');
         this.roll();
-    });
-    diebtn.addEventListener('click', () => {
-        this.roll ();
-    });
-    this.div.addEventListener('dblclick', () => {
-        this.deleteDie();
-    });
-}
-roll() {
-    let rollValue = randomVal(1, 7);
-    $(this.div).empty();
-    this.div.appendChild(document.createTextNode(`${rollValue}`));
-}
-deleteDie() {
-    this.div.remove();
-}
+        this.div.innerText = this.value;
+        diceArray.push(this)
+        this.div.style.left = `${x}px`;
+        this.div.style.top = `${y}px`;
+        this.div.style.border = "black, solid, 2px";
+        container.appendChild(this.div);
+
+        this.div.addEventListener('click', () => {
+            this.roll();
+        });
+        this.div.addEventListener('dblclick', () => {
+            this.deleteDie();
+        });
+    }
+    roll() {
+        let rollValue = randomVal(1, 7);
+        this.value = rollValue;
+        this.div.innerText = this.value;
+        //this.div.appendChild(document.createTextNode(`${rollValue}`));
+    }
+    deleteDie() {
+        this.div.remove();
+    }
 }
 let diebtn = document.getElementById("dieBtn");
-let btn2 = document.getElementById("rollBtn");
+rollBtn.addEventListener('click', () => {
+    diceArray.forEach(die => {
+        die.roll()
+    });
+});
 
 diebtn.addEventListener('click', createDie);
 
@@ -44,7 +51,3 @@ function createDie() {
 function randomVal(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
-
-
-
-
